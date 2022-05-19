bc37865a405e9f239ab11dc6a1280b04b905712f
CarlosNuAla <carlosnunezalayon@gmail.com>
2022-05-19 13:33:54 +0100

    Add change game.js

--- js/game.js
+++ js/game.js
@@ -1,3 +1,34 @@
+/* eslint-disable no-unused-vars */
+let canvas;
+let ctx;
+const fps = 30;
+
+const canvasX = 500;
+const canvasY = 500;
+let tileX, tileY;
+
+let tablero;
+const filas = 100;
+const columnas = 100;
+
+const white = '#FFFFFF';
+const black = '#000000';
+
 function initialize() {
-    console.log('CarGa');
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2D');
+
+    canvas.widht = canvasX;
+    canvas.height = canvasY;
+
+    tileX = Math.floor(canvasX / filas);
+    tileY = Math.floor(canvasY / columnas);
+
+    setInterval(function () {
+        principal();
+    }, 3000 / fps);
+}
+
+function principal() {
+    console.log('fotograma');
 }
