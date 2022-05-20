5ea92841643eb126cab5d6ce77c4fd095a4e4327
CarlosNuAla <carlosnunezalayon@gmail.com>
2022-05-20 14:17:24 +0100

    Delete agent.js, add iniBoard and change game.js

--- js/game.js
+++ js/game.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-self-assign */
 /* eslint-disable no-unused-vars */
 import { createArray } from './newArray.js';
-import { agent } from './agent.js';
+import { initializeBoard } from './iniBoard.js';
 
 let canvas;
 let ctx;
@@ -15,8 +15,31 @@ let board;
 const lines = 100;
 const columns = 100;
 
-const white = '#FFFFFF';
+const black = '#000000';
 const green = '#0aaf04';
+
+const Agent = function (x, y, status) {
+    this.x = x;
+    this.y = y;
+    this.status = status;
+    this.statusZone = this.status;
+    this.neighbour = [];
+
+    this.addNeighbours = (board, lines, columns) => {
+        let xNeig;
+        let yNeig;
+        for (let i = -1; i < 2; i++) {
+            for (let j = -1; j < 2; j++) {
+                xNeig = (this.x + j + columns) % columns;
+                yNeig = (this.y + i + lines) % lines;
+
+                if (i != 0 || j != 0) {
+                    this.neighbour.push(board[yNeig][xNeig]);
+                }
+            }
+        }
+    };
+};
 
 initialize();
 function initialize() {
@@ -30,6 +53,8 @@ function initialize() {
     tileY = Math.floor(canvasY / columns);
 
     board = createArray(lines, columns);
+
+    initializeBoard(board);
 
     setInterval(function () {
         principal();
